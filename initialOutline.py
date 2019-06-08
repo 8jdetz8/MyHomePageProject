f5fb905ae8c5242989c2acd21b7d8b9fd29615fc
JD <8jdetz8@gmail.com>
2019-06-08 10:00:23 -0400

    added initialOutline.py

---
+++ initialOutline.py
@@ -0,0 +1,19 @@
+#! python3
+# dailyDataScraper - Scrapes data from my favorite websites every morning,
+# presenting everything I need in a clean, concise way.
+
+import os, requests
+from selenium import webdriver
+
+#TODO: Tell me what day it is
+
+#TODO: Get the high temperature for midlothian for the day
+
+#TODO: Display a todo list of things I need to do(classes, hw, work)
+
+#TODO: Print info related to my money.
+
+#TODO: Get the top 3 posts from r/news and r/worldnews
+
+#TODO: Display everything in a better interface rather than just printing it.
+
