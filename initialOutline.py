14ace84709cb2ae9d8041f3762c05a6a8c3bfefa
JD <8jdetz8@gmail.com>
2019-06-16 10:57:00 -0400

    Added weather

--- initialOutline.py
+++ initialOutline.py
@@ -2,16 +2,24 @@
 # dailyDataScraper - Scrapes data from my favorite websites every morning,
 # presenting everything I need in a clean, concise way.
 
-import os, requests, webbrowser
+import os, requests, webbrowser, re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-#TODO: Tell me what day it is
 driver = webdriver.Chrome(executable_path='C:\Program Files\Chrome Driver\chromedriver.exe')
+
+#Done: Tell me what day it is
 driver.get('https://www.google.com/search?q=what+is+today%27s+date')
-html = BeautifulSoup(driver.page_source, "lxml")
-date = html.find('div' , {'class': 'vk_bk dDoNo'})
+dateHTML = BeautifulSoup(driver.page_source, "lxml")
+date = dateHTML.find('div' , {'class': 'vk_bk dDoNo'})
+
 #TODO: Get the high temperature for midlothian for the day
+driver.get('https://weather.com/weather/today/l/28feca8e43465556bc0c70403b638f1433c3c769a9a430f433ce81f027b5e112')
+weatherHTML = BeautifulSoup(driver.page_source, "lxml")
+weatherTable = weatherHTML.find_all('div' , {'class': 'today_nowcard-hilo'})
+highTempRegex = re.compile(r'\d\d\d?')
+Temps = highTempRegex.search(str(weatherTable))
+
 
 #TODO: Display a todo list of things I need to do(classes, hw, work)
 
@@ -21,5 +29,6 @@ date = html.find('div' , {'class': 'vk_bk dDoNo'})
 
 #TODO: Display everything in a better interface rather than just printing it.
 
-Print('Today\'s date is ' + date.text)
+print('Today\'s date is' + date.text)
+print('The high temperature today in Midlothian is ' + Temps[0])
 
