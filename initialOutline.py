581b4177c17e61b6cffa8db634b01eee3d710020
JD <8jdetz8@gmail.com>
2019-06-18 09:50:07 -0400

    Added top new music from /r/HHH

--- initialOutline.py
+++ initialOutline.py
@@ -2,30 +2,34 @@
 # dailyDataScraper - Scrapes data from my favorite websites every morning,
 # presenting everything I need in a clean, concise way.
 
-import os, requests, webbrowser, re
+import requests, webbrowser, re, time
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path='C:\Program Files\Chrome Driver\chromedriver.exe')
 
+#TODO: Put everything in functions so it looks prettier? maybe
+
 #Done: Tell me what day it is
+
+#TODO: Complete this using a time function instead of googling.
 driver.get('https://www.google.com/search?q=what+is+today%27s+date')
 dateHTML = BeautifulSoup(driver.page_source, "lxml")
 date = dateHTML.find('div' , {'class': 'vk_bk dDoNo'})
 
 #Done: Get the high temperature for midlothian for the day
+
 driver.get('https://weather.com/weather/today/l/28feca8e43465556bc0c70403b638f1433c3c769a9a430f433ce81f027b5e112')
 weatherHTML = BeautifulSoup(driver.page_source, "lxml")
 weatherTable = weatherHTML.find_all('div' , {'class': 'today_nowcard-hilo'})
 highTempRegex = re.compile(r'\d\d\d?')
 Temps = highTempRegex.search(str(weatherTable))
 
-
 #TODO: Display a todo list of things I need to do(classes, hw, work)
 
-#TODO: Print info related to my money.
-
 #Done: Get the top 3 posts from r/news and r/worldnews
+
 driver.get('https://www.reddit.com/r/news/')
 newsHTML = BeautifulSoup(driver.page_source, "lxml")
 newsTitles = newsHTML.find_all('h2' , {'class': 'yk4f6w-0 gRWfND'})
@@ -33,7 +37,45 @@ driver.get('https://www.reddit.com/r/worldnews/')
 worldnewsHTML = BeautifulSoup(driver.page_source, "lxml")
 worldnewsTitles = worldnewsHTML.find_all('h2' , {'class': 'yk4f6w-0 gRWfND'})
 
-#TODO: Get the name of any new songs/albums from HHH.
+#Done: Get any new music from reddit.com/r/hiphopheads
+def hiphopheadsPosts():
+    driver.get('https://www.reddit.com/r/hiphopheads/')
+    hiphopheadsHTML = BeautifulSoup(driver.page_source, "lxml")
+    h_all = hiphopheadsHTML.find_all('h2') #h_all is a bs4.element.resultset
+    result = []
+    for h in h_all:  #moving h_all text to a list
+        result.append(h.text)
+    freshRegex = re.compile(r'\[Fresh(\salbum)?\]', re.IGNORECASE | re.VERBOSE) #Only want fresh and fresh album tags.
+    freshStuff = list(filter(freshRegex.match, result)) #Removing everything without fresh
+    for i in range(len(freshStuff)):
+        if len(freshStuff) == 0: #check to see if anything in list.
+            print('No new music today.') 
+        else:
+            print(freshStuff[i])
+            if i == 2: #prints up to the first three matches.
+                break
+
+
+#UNFINISHED: Get info related to my money.
+#CANNOT SOLVE PROBLEM OF TWO FACTOR AUTHENTICATION AT THIS TIME
+#HAVE NOT TESTED AMERICAN EXPRESS YET. 
+
+#print('What is your union username?')
+#unionUsername = input()
+#print('What is your union password?')
+#unionPW = input()
+#driver.get('https://www.atlanticunionbank.com/')
+#unionLoginToggle = driver.find_element_by_id('loginToggle')
+#time.sleep(5)
+#unionLoginToggle.click()
+#time.sleep(1)
+#unionLoginInput = driver.find_element_by_id('userid')
+#unionLoginInput.send_keys(unionUsername)
+#time.sleep(1)
+#unionPasswordInput = driver.find_element_by_id('password')
+#unionPasswordInput.send_keys(unionPW)
+#time.sleep(1)
+#unionPasswordInput.send_keys(Keys.ENTER)
 
 #TODO: Display everything in a better interface rather than just printing it.
 
@@ -45,3 +87,4 @@ for i in range(3):
 print('\nIn World News: \n')
 for i in range(3):
     print(worldnewsTitles[i].getText())
+hiphopheadsPosts()
