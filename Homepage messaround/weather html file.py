76845f8158009df68772266f328ef95aab6982d4
JD <8jdetz8@gmail.com>
2019-07-01 11:55:21 -0400

    Added images to website, added articles, hyperlinks, comic.

--- Homepage messaround/weather html file.py
+++ Homepage messaround/weather html file.py
@@ -1,18 +1,20 @@
 #! python3
 # Homepage HTML Writer
 
-import requests, webbrowser, re, time
+import requests, webbrowser, random, os
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import datetime
 import praw
+from PIL import Image
+import urllib.request
 
 template = '''
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Styles Conference</title>
+    <title>My Home Page</title>
     <link rel="stylesheet" href="Stylesheets/newmain.css">
   </head>
   <header>
@@ -26,8 +28,16 @@ template = '''
             <h3><b>{date}</b></h3>
           </section>
       </section>
-      <section class="weather"
-          <h2>The high temperature in Midlothian today is high</h2>
+      <section class="weather">
+        <a href="https://weather.com/weather/hourbyhour/l/23113:4:US">
+        <img src = "C://Users/Student/Desktop/MyHomePageProject/Homepage messaround/sample_screenshot.png", alt = "weather table">
+        </a>
+      </section>
+      <section class="xkcd">
+        <a href="{comicUrl}"<p>Click here for a random XKCD Comic</p></a>
+      </section>
+      <section class="feeArticle">
+          <a href="https://fee.org/"<p>Click here to find an FEE Article</p></a>
       </section>
       <section class="hhhLabel">
           <h2>Recent Hip Hop releases:</h2>
@@ -41,17 +51,17 @@ template = '''
           <h2>In US News today:</h2>
       </section>
       <section class="newsData">
-          <p>{news0}</p>
-          <p>{news1}</p>
-          <p>{news2}</p>
+          <a href="{newsLink0}"<p>{news0}</p></a>
+          <a href="{newsLink1}"<p>{news1}</p></a>
+          <a href="{newsLink2}"<p>{news2}</p></a>
       </section>
       <section class="worldNewsLabel">
           <h2>In World News today:</h2>
       </section>
       <section class="worldNewsData">
-        <p>{world0}</p>
-        <p>{world1}</p>
-        <p>{world2}</p>
+        <a href="{worldNewsLink0}"<p>{world0}</p></a>
+        <a href="{worldNewsLink1}"<p>{world1}</p></a>
+        <a href="{worldNewsLink2}"<p>{world2}</p></a>
       </section>
   </html>
 '''
@@ -63,12 +73,19 @@ rn = datetime.datetime.today()
 #-------------------------------------------------------------------------
 #Getting the weather
 #-------------------------------------------------------------------------
-#driver = webdriver.Chrome(executable_path='C:\Program Files\Chrome Driver\chromedriver.exe')
+driver = webdriver.Chrome(executable_path='C:\\Users\Student\AppData\Local\Programs\Python\Python37-32\chromedriver\chromedriver.exe')
 #driver.get('https://weather.com/weather/today/l/28feca8e43465556bc0c70403b638f1433c3c769a9a430f433ce81f027b5e112')
 #weatherHTML = BeautifulSoup(driver.page_source, "lxml")
 #weatherTable = weatherHTML.find_all('div' , {'class': 'today_nowcard-hilo'})
 #highTempRegex = re.compile(r'\d\d\d?')
 #Temps = highTempRegex.search(str(weatherTable))
+driver.get('https://www.google.com/search?q=weather+in+midlothian+va')
+driver.save_screenshot('sample_screenshot.png')
+imageObject = Image.open("C://Users/Student/Desktop/MyHomePageProject/Homepage messaround/sample_screenshot.png")
+cropped = imageObject.crop((225, 295, 1150, 770))
+cropped.save("C://Users/Student/Desktop/MyHomePageProject/Homepage messaround/sample_screenshot.png")
+#925x473
+#1554x794
 #-------------------------------------------------------------------------
 #Setting up reddit data scraping
 #-------------------------------------------------------------------------
@@ -82,18 +99,22 @@ reddit = praw.Reddit(client_id = 'uhtUBYZOo53vCQ', \
 #-------------------------------------------------------------------------
 subreddit = reddit.subreddit('News')
 newsPosts = []
+newsLinks = []
 for post in subreddit.hot(limit=3): #only want 3 posts
     newsPosts.append(post.title)
+    newsLinks.append(post.url)
 subreddit = reddit.subreddit('WorldNews')
 worldNewsPosts = []
+worldNewsLinks = []
 for post in subreddit.hot(limit=3): #only want 3 posts
     worldNewsPosts.append(post.title)
+    worldNewsLinks.append(post.url)
 #-------------------------------------------------------------------------
 #Getting HHH Posts
 #-------------------------------------------------------------------------
 subreddit = reddit.subreddit('HipHopHeads')
 HHHPosts = []
-for post in subreddit.hot(limit=35): #looking through first 35 posts
+for post in subreddit.hot(limit=50): #looking through first 50 posts
     HHHPosts.append(post.title) #post.title only gets titles
 c = 0
 HHHFresh = []
@@ -103,12 +124,24 @@ for i in range(len(HHHPosts)):
         c += 1
     if c == 3: #stop after three posts
         break
+
+#-------------------------------------------------------------------------
+#Getting a random xkcd comic
+#-------------------------------------------------------------------------
+os.makedirs('xkcd', exist_ok=True)
+comicNum = random.randint(1,2168)
+driver.get("https://xkcd.com/" + str(comicNum))
+res = requests.get("https://xkcd.com/" + str(comicNum))
+soup = BeautifulSoup(res.text, features="lxml")
+comicElem = soup.select('#comic img')
+comicUrl = 'http:' + comicElem[0].get('src')
 #-------------------------------------------------------------------------
 #Establishing all values that will need to be displayed
 #-------------------------------------------------------------------------
+
 data = dict(
     date = rn.strftime('%A, %B %d %Y'),
-   # high = Temps[0],
+    high = 'high',
     news0 = newsPosts[0],
     news1 = newsPosts[1],
     news2 = newsPosts[2],
@@ -117,7 +150,14 @@ data = dict(
     world2 = worldNewsPosts[2],
     hhh0 = HHHFresh[0],
     hhh1 = HHHFresh[1],
-    hhh2 = HHHFresh[2]
+    hhh2 = HHHFresh[2],
+    newsLink0 = newsLinks[0],
+    newsLink1 = newsLinks[1],
+    newsLink2 = newsLinks[2],
+    worldNewsLink0 = worldNewsLinks[0],
+    worldNewsLink1 = worldNewsLinks[1],
+    worldNewsLink2 = worldNewsLinks[2],
+    comicUrl = comicUrl
 )
 #-------------------------------------------------------------------------
 #Inserting the values
@@ -125,5 +165,11 @@ data = dict(
 
 with open('todaysdate.html', 'w') as f:
     f.write(template.format(**data))
+
+#-------------------------------------------------------------------------
+#Closing the browser
+#-------------------------------------------------------------------------
+
+driver.quit()
 
 
